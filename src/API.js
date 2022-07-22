95e72bb2d4131e9e640215e75fce0b7e932f8305
rtk18 <mehra.ritik18@gmail.com>
2022-07-22 10:13:16 +0530

    changes

--- src/API.js
+++ src/API.js
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://127.0.0.1:3000',
-    timeout: 1000,
-})
+  baseURL: "http://127.0.0.1:3000",
+  timeout: 1000,
+});
 
-export { API }
+export { API };
